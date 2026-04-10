5841143234fde7382684486775f47c96a12a6a14
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2026-04-10 23:04:38 +0200

    refactor: update metadata and rendering scripts
    Refactor author information in _metadata.yml to include structured fields.
    Update render.sh to include additional posts for rendering and improve error handling.
    Upgrade renv version in lock files for better dependency management.

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 Rscript -e "renv::activate(profile = '2020-05-06-ggpacman')"
 Rscript -e "renv::restore()"
@@ -29,12 +29,17 @@ quarto render posts/2023-05-30-quarto-light-dark
 Rscript -e "renv::activate(profile = 'default')"
 Rscript -e "renv::restore()"
 quarto render posts/2024-12-30-quarto-github-pages
+quarto render posts/2025-04-21-quarto-revealjs-tabset-pdf
+quarto render posts/2025-05-19-quarto-codespaces
+quarto render posts/2025-10-20-quarto-wizard-1-0-0
+quarto render posts/2025-11-06-quarto-extensions-lua
+quarto render posts/2025-11-20-quarto-editor-settings
+quarto render posts/2025-12-12-quarto-extensions-updater
+quarto render posts/2026-01-12-quarto-wizard-2-0-0
+quarto render posts/2026-01-19-typst-document-dispatcher
+quarto render posts/2026-02-27-typst-template-tutorial-part1
+quarto render posts/2026-03-05-typst-template-tutorial-part2
 
-Rscript -e "renv::activate(profile = 'default')"
-Rscript -e "renv::restore()"
 quarto render
 
-Rscript -e "renv::activate(profile = 'default')"
-Rscript -e "renv::restore()"
-quarto render posts/2025-04-21-quarto-revealjs-tabset-pdf
 bash posts/2025-04-21-quarto-revealjs-tabset-pdf/assets/featured.sh
